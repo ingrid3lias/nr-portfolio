e51ff68905f3160df612f2c32d5b97dcfca15b73
Ingrid Elias <elias.ingrid@yahoo.com>
2016-02-01 22:13:27 +0900

    Integrated RSS feed directive and feedlist service

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-;(function(window){
-    angular.module('nickPortfolio', []);
-        .directive('articles', function(){
-            return {
-                restrict: 'E',
-                transclude: true,
-                scope: {},
-                templateUrl: 'articles.html',
-                link: function(scope, element, attr){}
-            }
-        })
-    
-})(window);
+var feeds = [];
+	
+angular.module('feedModule', ['ngResource'])
+	.factory('FeedLoader', function ($resource) {
+		return $resource('http://ajax.googleapis.com/ajax/services/feed/load', {}, {
+			fetch: { method: 'JSONP', params: {v: '1.0', callback: 'JSON_CALLBACK'} }
+		});
+	})
+	.service('FeedList', function ($rootScope, FeedLoader) {
+		this.get = function() {
+			var feedSources = [
+				{title: 'Slashdot', url: 'http://blog.honeyfeed.fm/feed/'},
+			];
+			if (feeds.length === 0) {
+				for (var i=0; i<feedSources.length; i++) {
+					FeedLoader.fetch({q: feedSources[i].url, num: 10}, {}, function (data) {
+						var feed = data.responseData.feed;
+						feeds.push(feed);
+					});
+				}
+			}
+			return feeds;
+		};
+	})
+	.controller('FeedCtrl', function ($scope, FeedList) {
+		$scope.feeds = FeedList.get();
+		$scope.$on('FeedList', function (event, data) {
+			$scope.feeds = data;
+		});
+	});
